eaaefefee80218f79ced3b44695e9ddc15d0978b
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-08 18:13:50 +0200

    First GUI Implementation

--- project.py
+++ project.py
@@ -1,6 +1,27 @@
+import PySimpleGUI as sg
+
 
 def main():
-    ...
+    # Elements inside the window
+    layout = [
+        [sg.Text("Test field for input:")],
+        [sg.Input("", key="Input1")],
+        [sg.Button("Search"), sg.Button("Exit")],
+    ]
+    # Window initialization
+    window = sg.Window("CS50P Project", layout, finalize=True)
+    window["Input1"].bind("<Return>", "_Enter")
+    # Event loop
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
+
+        if event == "Search" or event == "Input1" + "_Enter":
+            print("output:", values["Input1"])
+
+    window.close()
+
 
 if __name__ == "__main__":
     main()
