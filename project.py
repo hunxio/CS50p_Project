7e25618af1b3cacfc96a9c0bff69ce70e4a0b030
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-08 18:50:10 +0200

    Pop up window error if input is empty

--- project.py
+++ project.py
@@ -4,22 +4,37 @@ import PySimpleGUI as sg
 def main():
     # Elements inside the window
     layout = [
-        [sg.Text("Test field for input:")],
-        [sg.Input("", key="Input1")],
-        [sg.Button("Search"), sg.Button("Exit")],
+        [sg.Text("Welcome to my CS50P Project!")],
+        [sg.Text("User input field:"), sg.Input("", key="Input1")],
+        [sg.Button("Search"), sg.Button("Exit")]
     ]
+
     # Window initialization
     window = sg.Window("CS50P Project", layout, finalize=True)
+
+    # Bind Enter key to search button
     window["Input1"].bind("<Return>", "_Enter")
+
     # Event loop
     while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == "Exit" or event == "":
-            print("Exiting...")
+        user_input = values["Input1"].strip()
+        if event == sg.WIN_CLOSED or event == "Exit":
+            print("Exiting Applicaiton...")
             break
 
         if event == "Search" or event == "Input1" + "_Enter":
-            print("output:", values["Input1"])
+            if user_input == "" or user_input is None:
+                    layout = [[sg.Text("Something went wrong during the process...")], [sg.Text("Please enter a value in the search bar.")], [sg.Button("Exit")]]
+                    error_window = sg.Window("Error", layout, finalize=True)
+                    while True:
+                        event, _ = error_window.read()
+                        if event == sg.WIN_CLOSED or event == "Exit":
+                            break
+            else:
+                print("Your input was: ", user_input)
+                break
+
 
     window.close()
 
