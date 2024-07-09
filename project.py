916255d072b9ac868065b30d794e0605bfa83710
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-09 17:35:24 +0200

    error handling for False input

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ def main():
     # Elements inside the window
     layout = [
         [sg.Text("CS50P Weather Project", size=(16, 1), font=FONT+str(20))],
-        [sg.Text("User input field :", size=(12, 1), font=FONT+str(18)), sg.Input("", key="Input1", size=(25,3), font=FONT+str(18))],
+        [sg.Text("User input field :", size=(12, 1), font=FONT+str(18)), sg.Input("", key="Input1", size=(25,3), enable_events=True, font=FONT+str(18))],
         [sg.Button("Search", size=(20, 1), font=FONT+str(16)), sg.Button("Exit", size=(20, 1), font=FONT+str(16))]
     ]
 
@@ -21,24 +21,23 @@ def main():
     while True:
         event, values = window.read()
         # If app is closed without the user entering a value
-        try:
-            user_input = values["Input1"].strip()
-        except TypeError:
-            break
 
         if event == sg.WIN_CLOSED or event == "Exit":
             print("Exiting application.")
             break
 
         if event == "Search" or event == "Input1" + "_Enter":
-            # Pop up window when user does not enter a value
-            if not user_input:
-                no_input_popup()
-            else:
+            try:
+                user_input = values["Input1"].strip()
+                if not user_input:
+                    raise ValueError
                 print("Your input was: ", user_input)
+                window.close()
+            except ValueError:
+                # Pop up window when user does not enter a value
+                no_input_popup()
 
 
-    window.close()
 
 def no_input_popup():
     sg.popup("Error", "Please enter a valid input in the search bar.", font=FONT+str(18))
