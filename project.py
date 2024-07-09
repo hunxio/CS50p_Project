8ceb1ce88e40d006e1c9ff225cefe81947018a66
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-09 15:45:30 +0200

    Centered elements, added font and size to elements,

--- project.py
+++ project.py
@@ -4,13 +4,13 @@ import PySimpleGUI as sg
 def main():
     # Elements inside the window
     layout = [
-        [sg.Text("Welcome to my CS50P Project!")],
-        [sg.Text("User input field:"), sg.Input("", key="Input1")],
-        [sg.Button("Search"), sg.Button("Exit")]
+        [sg.Text("Welcome to my CS50P Project!", size=(25, 1), font="Arial")],
+        [sg.Text("User input field:", size=(11, 1), font="Arial"), sg.Input("", key="Input1")],
+        [sg.Button("Search", size=(22, 1), font="Arial"), sg.Button("Exit", size=(22, 1), font="Arial")]
     ]
 
     # Window initialization
-    window = sg.Window("CS50P Project", layout, finalize=True)
+    window = sg.Window("CS50P Project", layout, finalize=True, element_justification="c")
 
     # Bind Enter key to search button
     window["Input1"].bind("<Return>", "_Enter")
@@ -24,16 +24,19 @@ def main():
         except TypeError:
             break
         if event == sg.WIN_CLOSED or event == "Exit":
-            print("Exiting Applicaiton...")
+            print("Exiting Applicaiton.")
             break
 
         if event == "Search" or event == "Input1" + "_Enter":
+            # Pop up window when user does not enter a value
             if user_input == "" or user_input is None:
-                    layout = [[sg.Text("Something went wrong during the process...")], [sg.Text("Please enter a value in the search bar.")], [sg.Button("Exit")]]
-                    error_window = sg.Window("Error", layout, finalize=True)
+                    layout = [[sg.Text("Something went wrong during the process...", size=(30, 1), font="Arial")], 
+                    [sg.Text("Please enter a value in the search bar.", size=(30, 1), font="Arial")], 
+                    [sg.Button("Close", size=(20, 1), font="Arial")]]
+                    error_window = sg.Window("Error", layout, finalize=True, element_justification="c")
                     while True:
                         event, _ = error_window.read()
-                        if event == sg.WIN_CLOSED or event == "Exit":
+                        if event == sg.WIN_CLOSED or event == "Close":
                             break
             else:
                 print("Your input was: ", user_input)
