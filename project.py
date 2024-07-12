59ff6ee82cd92fe17438322941cbb8932bf8fef8
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-12 17:21:42 +0200

    Added get_time to retrieve user's local time, added get_coordinates to retrieve user's longitude and latitude(needs to be fixed)

--- project.py
+++ project.py
@@ -3,7 +3,8 @@ import openmeteo_requests
 import requests_cache
 import pandas as pd
 from retry_requests import retry
-
+from datetime import datetime
+from geopy.geocoders import Nominatim
 
 # Used as follows: FONT+str(<number>), <number> increases or decreases the font fize
 FONT = "Montserrat "
@@ -47,7 +48,9 @@ def main():
                 user_input = values["Input1"].strip()
                 if not user_input:
                     raise ValueError
-                print(api())
+                temperature, humidity, precipitation, time = api()[0], api()[1], api()[2], get_time()
+                get_coordinates(user_input)
+                print(get_coordinates(capitalize(user_input))[0], get_coordinates(capitalize(user_input))[1])
                 window.close()
             except ValueError:
                 # Pop up window when user does not enter a value
@@ -62,15 +65,16 @@ def no_input_popup():
 
 # TODO: Configure the API to return informations about a city. Temperature, humidity and precipitation (Might change them later on)
 def api():
-    # Openmeteo API request
+    # Cache the requests to improve performance and reduce the number of API calls.
     cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
     openmeteo = openmeteo_requests.Client(session=retry_session)
 
+    # Openmeteo API request
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
-        "latitude": 52.52,
-        "longitude": 13.41,
+        "latitude": 55.7522, # needs to change based on location
+        "longitude": 37.6156, # needs to change based on location
         "current": ["temperature_2m", "relative_humidity_2m", "precipitation"],
     }
     responses = openmeteo.weather_api(url, params=params)
@@ -81,14 +85,24 @@ def api():
     current_temperature_2m = current.Variables(0).Value()
     current_relative_humidity_2m = current.Variables(1).Value()
     current_precipitation = current.Variables(2).Value()
-    return (
-        f"Current time {current.Time()}\nCurrent temperature_2m {current_temperature_2m}\nCurrent relative_humidity_2m {current_relative_humidity_2m}\nCurrent precipitation {current_precipitation}"
-    )
 
-#TODO: Time to show hh:mm format
-def time_format(time: int) -> str:
-    ...
+    return current_temperature_2m, current_relative_humidity_2m, current_precipitation
 
+
+# Get time from user's location
+def get_time():
+    c = datetime.now()
+    current_time = c.strftime("%H:%M")
+    return current_time
+
+# TODO: Fix geopy
+def get_coordinates(city: str):
+    loc = Nominatim(user_agent="GetLoc")
+ 
+# entering the location name
+    getLoc = loc.geocode(f"{city}")
+
+    return getLoc.latitude, getLoc.longitude
 
 
 if __name__ == "__main__":
