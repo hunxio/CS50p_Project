452a98c1b92438329a92ad9571627776bd0abf85
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-14 15:13:23 +0200

    Removed whitespaces from FONT

--- project.py
+++ project.py
@@ -10,17 +10,18 @@ from geopy.geocoders import Nominatim
 
 
 # Used as follows: FONT+str(<number>), <number> increases or decreases the font fize
-FONT = "Montserrat "
+FONT = "Montserrat"
 
 
 def main():
+    global user_loc_input
     window = tk.Tk()
     window.geometry("500x300")
     window.title("CS50P Weather Project")
     window.grid_columnconfigure(0, weight=1)
     location_text = tk.Label(window,
                          text="Please select a location",
-                         font=(FONT.strip, 15))
+                         font=(FONT, 15))
     location_text.grid(row=0, column=0, sticky="WE", padx=20, pady=10)
     user_loc_input = tk.Entry()
     user_loc_input.grid(row=1, column=0, sticky="WE", padx=10)
@@ -28,13 +29,15 @@ def main():
     confirm_button.grid(row=2, column=0, sticky="N", pady=10)
     window.mainloop()
 
-
+#TODO: Fix user input validation error
 def input_validation():
+    user_input = user_loc_input.get()
     if user_input():
-        output_response= user_input.get()
+        output_response= user_input
     else:
         output_response = "The search bar is empty, please enter your search"
 
+    print(f"Test output: {user_i}")
 
 def temperature_api():
     # Cache the requests to improve performance and reduce the number of API calls.
