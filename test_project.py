8aca39ef610d3f1d00eee54b5c146f20ebcd5280
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-23 16:24:14 +0200

    updated test_project

--- test_project.py
+++ test_project.py
@@ -1,13 +1,8 @@
 import pytest
-from project import get_coordinates, validation
+from project import get_coordinates
 
 # Testing get_coordinates if returns value when called
-def test_new_york_coordinates():
+def test_get_coordinates():
     assert get_coordinates("New York") == {"latitude": 40.7127281, "longitude": -74.0060152}
 
-def test_london_coordinates():
-    assert get_coordinates("London") == {"latitude": 51.5074456, "longitude": -0.1277653}
-
-def test_rome_coordinates():
-    assert get_coordinates("Rome") == {"latitude": 41.8933203, "longitude": 12.4829321}
 
