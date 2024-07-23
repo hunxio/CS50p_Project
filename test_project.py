cbda496c04f81f451df62402364378ba88c1a496
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-23 16:19:08 +0200

    snake case formatting

--- test_project.py
+++ test_project.py
@@ -1,13 +1,13 @@
 import pytest
-from project import get_coordinates
+from project import get_coordinates, validation
 
 # Testing get_coordinates if returns value when called
-def test_NYCoordinates():
+def test_new_york_coordinates():
     assert get_coordinates("New York") == {"latitude": 40.7127281, "longitude": -74.0060152}
 
-def test_LondonCoordinates():
+def test_london_coordinates():
     assert get_coordinates("London") == {"latitude": 51.5074456, "longitude": -0.1277653}
 
-def test_RomeCoordinates():
+def test_rome_coordinates():
     assert get_coordinates("Rome") == {"latitude": 41.8933203, "longitude": 12.4829321}
 
